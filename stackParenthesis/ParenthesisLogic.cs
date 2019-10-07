70c9ecf5a99dd051f34881cb18cafa93c8f51853
humbertojacobm <humbertojacobm@gmail.com>
2019-10-07 16:09:59 -0500

    using singleton pattern to encapsule the main logic

---
+++ stackParenthesis/ParenthesisLogic.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stackParenthesis
+{
+    public sealed class ParenthesisLogic
+    {
+        public ParenthesisLogic()
+        {
+
+        }
+
+        private static readonly object _padlock = new object();
+
+        private static ParenthesisLogic _instance = null;
+
+        public static ParenthesisLogic Instance
+        {
+            get
+            {
+                lock(_padlock)
+                {
+                    if(_instance == null)
+                    {
+                        _instance = new ParenthesisLogic();
+                    }
+                    return _instance;
+                }
+            }
+        }
+
+        public bool isBalancedExpression(string expression)
+        {
+
+            List<char> parenthList = (expression.ToList()).Where(x => isOpenKey(x.ToString()) || isCloseKey(x.ToString())).ToList();
+            if (parenthList.Count % 2 != 0)
+            {
+                return false;
+            }
+            Queue<char> justParenthesisFamily = new Queue<char>(parenthList);
+            Stack<char> openParenth = new Stack<char>();
+            char first = justParenthesisFamily.Dequeue();
+            char last = justParenthesisFamily.Last();
+            if (!(isOpenKey(first.ToString()) && isCloseKey(last.ToString())))
+            {
+                return false;
+            }
+
+            openParenth.Push(first);
+
+            while (justParenthesisFamily.Count > 0 || openParenth.Count > 0)
+            {
+                var familyTop = justParenthesisFamily.Peek();
+                if (isCloseKey(familyTop.ToString()))
+                {
+                    if (isCoupleComparisson(openParenth.Peek().ToString(), familyTop.ToString()))
+                    {
+                        justParenthesisFamily.Dequeue();
+                        openParenth.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    openParenth.Push(justParenthesisFamily.Dequeue());
+                }
+            }
+
+            if (justParenthesisFamily.Count > 0 || openParenth.Count > 0)
+            {
+                return false;
+            }
+            else
+
+                return true;
+        }
+
+        private bool isCoupleComparisson(string girlfriend, string boyfriend)
+        {
+            var isCouple = false;
+            switch (girlfriend)
+            {
+                case "{":
+                    if (boyfriend == "}")
+                    {
+                        isCouple = true;
+                    }
+                    break;
+                case "[":
+                    if (boyfriend == "]")
+                    {
+                        isCouple = true;
+                    }
+                    break;
+                case "(":
+                    if (boyfriend == ")")
+                    {
+                        isCouple = true;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return isCouple;
+        }
+
+        private bool isOpenKey(string key)
+        {
+            var isOpen = false;
+            switch (key)
+            {
+                case "{":
+                    isOpen = true;
+                    break;
+                case "[":
+                    isOpen = true;
+                    break;
+                case "(":
+                    isOpen = true;
+                    break;
+                default:
+                    break;
+            }
+            return isOpen;
+        }
+        private bool isCloseKey(string key)
+        {
+            var isOpen = false;
+            switch (key)
+            {
+                case "}":
+                    isOpen = true;
+                    break;
+                case "]":
+                    isOpen = true;
+                    break;
+                case ")":
+                    isOpen = true;
+                    break;
+                default:
+                    break;
+            }
+            return isOpen;
+        }
+
+
+
+    }
+}
--- stackParenthesis/Program.cs
+++ stackParenthesis/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
-using System.Collections.Generic;
 
 namespace stackParenthesis
 {
@@ -36,7 +35,7 @@ namespace stackParenthesis
 
             Console.WriteLine(expresion1);
 
-            isBalance = isBalancedExpression(expresion1);
+            isBalance = ParenthesisLogic.Instance.isBalancedExpression(expresion1);
 
             Console.WriteLine("Es balanceada ?: "+ isBalance.ToString());
             //foreach (var item in justParenthesisFamily)
@@ -47,121 +46,5 @@ namespace stackParenthesis
             Console.ReadKey();
         }
 
-        private static bool isBalancedExpression(string expression)
-        {
-                  
-            List<char> parenthList = (expression.ToList()).Where(x => isOpenKey(x.ToString()) || isCloseKey(x.ToString())).ToList();
-            if (parenthList.Count%2!=0)
-            {
-                return false;
-            }
-            Queue<char> justParenthesisFamily = new Queue<char>(parenthList);
-            Stack<char> openParenth = new Stack<char>();
-            char first = justParenthesisFamily.Dequeue();
-            char last = justParenthesisFamily.Last();
-            if (!(isOpenKey(first.ToString()) && isCloseKey(last.ToString())))
-            {
-                return false;
-            }
-
-            openParenth.Push(first);
-
-            while(justParenthesisFamily.Count>0 || openParenth.Count>0)
-            {
-                var familyTop = justParenthesisFamily.Peek();
-                if (isCloseKey(familyTop.ToString()))
-                {                    
-                    if(isCoupleComparisson(openParenth.Peek().ToString(), familyTop.ToString()))
-                    {
-                        justParenthesisFamily.Dequeue();
-                        openParenth.Pop();                        
-                    }
-                    else
-                    {
-                        return false;
-                    }
-                }
-                else
-                {
-                    openParenth.Push(justParenthesisFamily.Dequeue());
-                }
-            }
-
-            if (justParenthesisFamily.Count > 0 || openParenth.Count > 0)
-            {
-                return false;
-            }
-            else
-
-            return true;
-        }
-
-        private static bool isCoupleComparisson(string girlfriend, string boyfriend)
-        {
-            var isCouple = false;
-            switch (girlfriend)
-            {
-                case "{":
-                    if(boyfriend=="}")
-                    {
-                        isCouple = true;
-                    }
-                    break;
-                case "[":
-                    if (boyfriend == "]")
-                    {
-                        isCouple = true;
-                    }
-                    break;
-                case "(":
-                    if (boyfriend == ")")
-                    {
-                        isCouple = true;
-                    }
-                    break;
-                default:
-                    break;
-            }
-            return isCouple;
-        }
-
-        private static bool isOpenKey(string key)
-        {
-            var isOpen = false;
-            switch (key)
-            {
-                case "{":
-                    isOpen = true;
-                    break;
-                case "[":
-                    isOpen = true;
-                    break;
-                case "(":
-                    isOpen = true;
-                    break;
-                default:
-                    break;
-            }
-            return isOpen;
-        }
-        private static bool isCloseKey(string key)
-        {
-            var isOpen = false;
-            switch (key)
-            {
-                case "}":
-                    isOpen = true;
-                    break;
-                case "]":
-                    isOpen = true;
-                    break;
-                case ")":
-                    isOpen = true;
-                    break;
-                default:
-                    break;
-            }
-            return isOpen;
-        }
     }
 }
