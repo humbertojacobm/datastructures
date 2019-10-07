1588cb098d709ed600573727d447008af9241913
humbertojacobm <humbertojacobm@gmail.com>
2019-10-07 16:53:44 -0500

    factory

---
+++ stackParenthesisFactory/FactoryPattern.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stackParenthesisFactory
+{
+    public abstract class Checker
+    {
+        public abstract string type { get; }
+        public abstract string expression { get; set; }
+        public abstract bool isBalanced { get; }
+    }
+
+    public class HmoriChecker : Checker
+    {
+        private readonly string _type;
+        private string _expression;        
+
+        public HmoriChecker(string expression)
+        {
+            this._type = "hmori";
+            this._expression = expression;            
+        }
+
+        public override string type
+        {
+            get { return this._type; }
+        }
+
+        public override string expression
+        {
+            get { return this._expression; }
+            set { this._expression = value; }
+        }
+
+        public override bool isBalanced
+        {
+            get { return isBalancedExpression(_expression); }
+        }
+
+        public bool isBalancedExpression(string expression)
+        {
+
+            List<char> parenthList = (expression.ToList()).Where(x => isOpenKey(x.ToString()) || isCloseKey(x.ToString())).ToList();
+            if (parenthList.Count % 2 != 0)
+            {
+                return false;
+            }
+            Queue<char> justParenthesisFamily = new Queue<char>(parenthList);
+            Stack<char> openParenth = new Stack<char>();
+            char first = justParenthesisFamily.Dequeue();
+            char last = justParenthesisFamily.Last();
+            if (!(isOpenKey(first.ToString()) && isCloseKey(last.ToString())))
+            {
+                return false;
+            }
+
+            openParenth.Push(first);
+
+            while (justParenthesisFamily.Count > 0 || openParenth.Count > 0)
+            {
+                var familyTop = justParenthesisFamily.Peek();
+                if (isCloseKey(familyTop.ToString()))
+                {
+                    if (isCoupleComparisson(openParenth.Peek().ToString(), familyTop.ToString()))
+                    {
+                        justParenthesisFamily.Dequeue();
+                        openParenth.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    openParenth.Push(justParenthesisFamily.Dequeue());
+                }
+            }
+
+            if (justParenthesisFamily.Count > 0 || openParenth.Count > 0)
+            {
+                return false;
+            }
+            else
+
+                return true;
+        }
+
+        private bool isCoupleComparisson(string girlfriend, string boyfriend)
+        {
+            var isCouple = false;
+            switch (girlfriend)
+            {
+                case "{":
+                    if (boyfriend == "}")
+                    {
+                        isCouple = true;
+                    }
+                    break;
+                case "[":
+                    if (boyfriend == "]")
+                    {
+                        isCouple = true;
+                    }
+                    break;
+                case "(":
+                    if (boyfriend == ")")
+                    {
+                        isCouple = true;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return isCouple;
+        }
+
+        private bool isOpenKey(string key)
+        {
+            var isOpen = false;
+            switch (key)
+            {
+                case "{":
+                    isOpen = true;
+                    break;
+                case "[":
+                    isOpen = true;
+                    break;
+                case "(":
+                    isOpen = true;
+                    break;
+                default:
+                    break;
+            }
+            return isOpen;
+        }
+        private bool isCloseKey(string key)
+        {
+            var isOpen = false;
+            switch (key)
+            {
+                case "}":
+                    isOpen = true;
+                    break;
+                case "]":
+                    isOpen = true;
+                    break;
+                case ")":
+                    isOpen = true;
+                    break;
+                default:
+                    break;
+            }
+            return isOpen;
+        }
+
+    }
+
+    public abstract class CheckerFactory
+    {
+        public abstract Checker GetChecker();
+    }
+
+    public class HmoriCheckerFactory : CheckerFactory
+    {
+        private string _expression;       
+        public HmoriCheckerFactory(string expression)
+        {            
+            this._expression = expression;            
+        }
+
+        public override Checker GetChecker()
+        {
+            return new HmoriChecker(_expression);
+        }
+    }
+
+}
---
+++ stackParenthesisFactory/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stackParenthesisFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var expresion1 = "[()]{}{[()()]()}";            
+            //var expresion1 = "[(])";
+            
+            Console.WriteLine(expresion1);                
+
+            Checker checker = new HmoriCheckerFactory(expresion1).GetChecker();            
+
+            Console.WriteLine("Es balanceada ?: " + checker.isBalanced.ToString());            
+
+            Console.ReadKey();
+        }
+    }
+}
